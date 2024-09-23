415399f6efc68a631bf6763319552271a2228988
tobfer <soutobias@yahoo.com.br>
2024-09-23 12:46:31 +0100

    bug: correct rechunk strategy, add dask to checksum calculation and add flag to skip data integrity check

--- src/msm_os/sanity_checks.py
+++ src/msm_os/sanity_checks.py
@@ -318,7 +318,8 @@ def _calculate_checksum(expected_checksum: int,
         dtype = var_part_of_ds_dataset.data.dtype    
         if np.issubdtype(dtype, np.number):
             if isinstance(var_part_of_ds_dataset.data, dask.array.core.Array):
-                data_array = var_part_of_ds_dataset.data.astype(np.uint32)
+                data_array = part_of_ds_dataset[var].data
+                data_array = dask.array.nan_to_num(data_array, nan=0, posinf=0, neginf=0).astype(np.uint32)
             else:
                 values = var_part_of_ds_dataset.values
                 values = np.nan_to_num(values, nan=0, posinf=0, neginf=0).astype(np.uint32)
