162b8bfc83e9e483d6266e6b936a09d87c1d6ff4
Ollie Tooth <oliver.tooth@noc.ac.uk>
2025-07-09 16:06:48 +0100

    Added OceanDataCatalog class to interact with NOC STAC.

--- OceanDataStore/__init__.py
+++ OceanDataStore/__init__.py
@@ -1,4 +1,6 @@
 """OceanDataStore base package."""
 __version__ = "0.1.0"
-__author__ = "Joao Morado, Tobias Ferreira, Ollie Tooth"
+__author__ = "Ollie Tooth, Joao Morado, Tobias Ferreira"
 __credits__ = "National Oceanography Centre"
+
+from .data_catalog import OceanDataCatalog
