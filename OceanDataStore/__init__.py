5b4b9131016894b269da31c19dbc080e2e8f4870
Ollie Tooth <oliver.tooth@noc.ac.uk>
2026-02-24 11:37:24 +0000

    Add dyanmic versioning & update metadata in __init__.py

--- OceanDataStore/__init__.py
+++ OceanDataStore/__init__.py
@@ -1,6 +1,21 @@
-"""OceanDataStore base package."""
-__version__ = "0.1.0"
-__author__ = "Ollie Tooth, Joao Morado, Tobias Ferreira"
-__credits__ = "National Oceanography Centre"
+"""
+OceanDataStore
 
-from .data_catalog import OceanDataCatalog
+An open-source Python library to streamline writing, updating and accessing
+ocean data stored in cloud object storage.
+"""
+__author__ = "Ollie Tooth, Joao Morado, Tobias Ferreira"
+__credits__ = "National Oceanography Centre (NOC), Southampton, UK"
+
+from importlib.metadata import version as _version
+
+from OceanDataStore.data_catalog import OceanDataCatalog
+
+try:
+    __version__ = _version("OceanDataStore")
+except Exception:
+    # Local copy or not installed with setuptools.
+    # Disable minimum version checks on downstream libraries.
+    __version__ = "9999"
+
+__all__ = ("OceanDataCatalog")
